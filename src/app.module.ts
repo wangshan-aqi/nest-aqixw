0f7c739960892d4fc015c5b3965b1ee417798208
shan.wang <shan.wang@linkedcare.cn>
2023-06-01 11:17:51 +0800

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BannerModule } from './banner/banner.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "Chang521",
-      database: "test",
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true
-    }),
-    BannerModule
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
